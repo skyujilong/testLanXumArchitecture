bbb96731e39d23aec9efc47dea0d8af3343b7445
yujilong <skyujilong@163.com>
2014-11-24 15:38:00 +0800

    test requirejs and art template

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ testLanXumArchitecture
 ======================
 
 前端建构测试
+应用前端框架有如下这些：
+requirejs
+backbone
+artTemplete
+tmodjs
+
---
+++ js/require-config.js
@@ -0,0 +1,13 @@
+/**
+ * Created by lsc on 2014/11/24.
+ */
+//requirejs配置文件
+require.config({
+    baseUrl:'js/testRequirejs',
+    paths:{
+        'jquery':'../jquery-1.9.1.min',
+        'template':'../template',
+        'domReady':'../domReady',
+        'template-debug':'../template-debug'
+    }
+});
